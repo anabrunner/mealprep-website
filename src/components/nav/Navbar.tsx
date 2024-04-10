03f70b650141c6173227097f590262133b2155da
Ana Brunner <anabrunner.ab@gmail.com>
2024-04-09 21:11:36 -0700

    feat: add responsive behaviour to navbar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -7,23 +7,33 @@ export interface INavbar {}
 // eslint-disable-next-line no-empty-pattern
 export function Navbar({}: INavbar) {
   return (
-    <div className={styles.navbar}>
-      <Image src={logoPic} alt="PrepPotato Logo" width={100} />
-      <h1 className="font-headingFont">PrepPotato</h1>
-      <ul className="flex justify-end w-screen p-5 space-x-6">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="about">About Us</Link>
-        </li>
-        <li>
-          <Link href="blog">Blog</Link>
-        </li>
-        <li>
-          <Link href="login">Login</Link>
-        </li>
-      </ul>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logoHeader}>
+          <Image src={logoPic} alt="PrepPotato Logo" width={100} />
+          <h1 className="font-headingFont">PrepPotato</h1>
+        </div>
+        <input type="checkbox" name="" id="" />
+        <div className={styles.hamburgerLines}>
+          <span className={styles.line1}></span>
+          <span className={styles.line2}></span>
+          <span className={styles.line3}></span>
+        </div>
+        <ul className={styles.menuLinks}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="about">About Us</Link>
+          </li>
+          <li>
+            <Link href="blog">Blog</Link>
+          </li>
+          <li className={styles.loginBtn}>
+            <Link href="login">Login</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
